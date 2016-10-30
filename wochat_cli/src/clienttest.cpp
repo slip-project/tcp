8112ac4cacc9b6112fd25c90cbde8fdb62036429
Souler Ou <ou@souler.me>
2016-10-30 12:14:35 +0800

    clienttrial6

--- wochat_cli/src/clienttest.cpp
+++ wochat_cli/src/clienttest.cpp
@@ -115,7 +115,7 @@ int main(int argc, char const *argv[]) {
 			userName = "";
 
 		} else if (instruction == "send" && isLogin) {
-			std::string tempname , tempcontent , useless;
+			std::string tempname , tempcontent;
 			std::stringstream stream;
 
 			cout << "Please Input Your Friend\'s Username:" << endl;
@@ -123,7 +123,7 @@ int main(int argc, char const *argv[]) {
 			cin >> tempname;
 			cout << "Please Input the Message and press enter:" << endl;
 			cout << currentUser << " >> ";
-			cin >> useless;
+
 			std::getline(cin , tempcontent);
 
 			stream << "SEND " << userName << " " << tempname << " " << tempcontent << endl;
